ae3b40b93f31617264e8eb0a0d3b1053e0427f15
Ani Jekova <angelina.jekova@rubberduck.xyz>
2020-11-19 14:54:16 +0200

    filters for meals added

--- pages/CategoryMealsScreen.tsx
+++ pages/CategoryMealsScreen.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MEALS } from '../data/dummy-data';
+
+import AppContext from '../AppContext';
 
 import MealList from '../components/MealList';
 
 const CategoryMealsScreen = ({ route, navigation }) => {
+  const { filter } = useContext(AppContext);
   const categoryId = route.params?.categoryId ?? '';
 
-  const displayedMeals = MEALS.filter((meals) =>
-    meals.categoryIds.includes(categoryId),
+  const filteredMealsByCategory = MEALS.filter((meal) =>
+    meal.categoryIds.includes(categoryId),
   );
 
-  return <MealList list={displayedMeals} navigation={navigation} />;
+  let filteredMeals;
+
+  if (
+    !filter.isGlutenFree &&
+    !filter.isLactoseFree &&
+    !filter.isVegan &&
+    !filter.isVegetarian
+  ) {
+    filteredMeals = filteredMealsByCategory;
+  } else {
+    if (filter.isGlutenFree) {
+      filteredMeals = filteredMealsByCategory.filter(
+        (meal) => meal.isGlutenFree === filter.isGlutenFree,
+      );
+    }
+
+    if (filter.isLactoseFree) {
+      filteredMeals = filteredMealsByCategory.filter(
+        (meal) => meal.isLactoseFree === filter.isLactoseFree,
+      );
+    }
+
+    if (filter.isVegan) {
+      filteredMeals = filteredMealsByCategory.filter(
+        (meal) => meal.isVegan === filter.isVegan,
+      );
+    }
+
+    if (filter.isVegetarian) {
+      filteredMeals = filteredMealsByCategory.filter(
+        (meal) => meal.isVegetarian === filter.isVegetarian,
+      );
+    }
+  }
+
+  return <MealList list={filteredMeals} navigation={navigation} />;
 };
 
 export default CategoryMealsScreen;
